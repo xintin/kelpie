d369c65c96d50a48aea5de006c493d7ffaf04991
xintin <gaurav.verma@stonybrook.edu>
2024-04-24 12:04:26 -0400

    added tiling expt for pluto (mm kernel)

--- matrix_multiplication/src/simple_comparison.cpp
+++ matrix_multiplication/src/simple_comparison.cpp
@@ -46,6 +46,32 @@ void matrix_multiply_tiled_pluto(const std::vector<std::vector<double>> &A,
   }
 }
 
+void matrix_multiply_tiled_pluto_expt(const std::vector<std::vector<double>> &A,
+                                      const std::vector<std::vector<double>> &B,
+                                      std::vector<std::vector<double>> &C,
+                                      const int T4, const int T5,
+                                      const int T6) {
+
+  int lbv, ubv;
+  int t1, t2, t3, t4, t5, t6;
+
+  for (t1 = 0; t1 <= (M - 1) / T4; t1++) {
+    for (t2 = 0; t2 <= (N - 1) / T5; t2++) {
+      for (t3 = 0; t3 <= (K - 1) / T6; t3++) {
+        for (t4 = T4 * t1; t4 <= std::min(M - 1, T4 * t1 + T4 - 1); t4++) {
+          for (t5 = T6 * t3; t5 <= std::min(K - 1, T6 * t3 + T6 - 1); t5++) {
+            lbv = T5 * t2;
+            ubv = std::min(N - 1, T5 * t2 + T5 - 1);
+            for (t6 = lbv; t6 <= ubv; t6++) {
+              C[t4][t6] += A[t4][t5] * B[t5][t6];
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 void matrix_multiply_tiled_ioopt(const std::vector<std::vector<double>> &A,
                                  const std::vector<std::vector<double>> &B,
                                  std::vector<std::vector<double>> &C) {
@@ -76,6 +102,8 @@ void matrix_multiply_tiled_ioopt(const std::vector<std::vector<double>> &A,
   }
 }
 
+bool isValid() { return true; }
+
 int main() {
   std::vector<std::vector<double>> A(M, std::vector<double>(K, 1.0));
   std::vector<std::vector<double>> B(K, std::vector<double>(N, 2.0));
@@ -95,7 +123,8 @@ int main() {
             << " seconds.\n";
 
   start = std::chrono::high_resolution_clock::now();
-  matrix_multiply_tiled_pluto(A, B, C_tiled);
+  // matrix_multiply_tiled_pluto(A, B, C_tiled);
+  matrix_multiply_tiled_pluto_expt(A, B, C_tiled, 32, 32, 32);
   stop = std::chrono::high_resolution_clock::now();
 
   duration =
@@ -104,6 +133,60 @@ int main() {
   std::cout << "Time taken by pluto tiled matrix multiplication: " << duration
             << " seconds.\n";
 
+  if (C == C_tiled) {
+    std::cout << "The results are same.\n";
+  } else {
+    std::cout << "The results differ.\n";
+  }
+
+  C_tiled.assign(C_tiled.size(), std::vector<double>(C_tiled[0].size(), 0.0));
+  start = std::chrono::high_resolution_clock::now();
+  // matrix_multiply_tiled_pluto(A, B, C_tiled);
+  matrix_multiply_tiled_pluto_expt(A, B, C_tiled, 128, 128, 16);
+  stop = std::chrono::high_resolution_clock::now();
+
+  duration =
+      std::chrono::duration_cast<std::chrono::duration<double>>(stop - start)
+          .count();
+  std::cout << "Time taken by pluto tiled matrix multiplication: " << duration
+            << " seconds.\n";
+
+  if (C == C_tiled) {
+    std::cout << "The results are same.\n";
+  } else {
+    std::cout << "The results differ.\n";
+  }
+
+  std::cout << "Start the experiment for max Tile size: 512\n";
+  std::vector<int> search_candidates = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+  for (auto &ts4 : search_candidates) {
+    for (auto &ts5 : search_candidates) {
+      for (auto &ts6 : search_candidates) {
+        C_tiled.assign(C_tiled.size(),
+                       std::vector<double>(C_tiled[0].size(), 0));
+        start = std::chrono::high_resolution_clock::now();
+        matrix_multiply_tiled_pluto_expt(A, B, C_tiled, ts4, ts5, ts6);
+        stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::duration<double>>(
+                       stop - start)
+                       .count();
+
+        if (C == C_tiled) {
+          std::cout << ts4 << " " << ts5 << " " << ts6 << " " << duration
+                    << "\n";
+        } else {
+          std::cout << "-------------\nThe results differ: ";
+          std::cout << ts4 << " " << ts5 << " " << ts6 << " " << duration
+                    << "\n";
+          std::cout << "-------------\n";
+        }
+      }
+    }
+  }
+
+  /* Comment out the tiled ioopt for the experimentation */
+  /*
   start = std::chrono::high_resolution_clock::now();
   matrix_multiply_tiled_ioopt(A, B, C_tiled_ioopt);
   stop = std::chrono::high_resolution_clock::now();
@@ -113,12 +196,7 @@ int main() {
           .count();
   std::cout << "Time taken by ioopt tiled matrix multiplication: " << duration
             << " seconds.\n";
-
-  if (C == C_tiled) {
-    std::cout << "The results of both multiplication methods are identical.\n";
-  } else {
-    std::cout << "The results differ.\n";
-  }
+  */
 
   return 0;
 }
