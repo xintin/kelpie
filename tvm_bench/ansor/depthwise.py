1d792b48c0cd672e9875c24618b2820715ed9e49
canesche <michael.canesche@gmail.com>
2024-05-16 18:14:44 -0300

    adding ansor benchmarks

---
+++ tvm_bench/ansor/depthwise.py
@@ -0,0 +1,101 @@
+import os, sys, time, argparse, tvm
+from tvm import te, auto_scheduler, topi
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src.utils import *
+
+## ------------------ Global ---------------------
+# 128 84 83 83 5  5  2       SAME
+# N   CI H  W  KH KW Strides Padding
+input_shape = (128, 84, 83, 83)
+filter_shape = (84, 1, 5, 5)
+strides = (1, 1)
+padding = (1, 1)
+dilation = (1, 1)
+layout = "NCHW"
+dtype = "float32"
+
+
+## ----------------- Benchmark -------------------
+@auto_scheduler.register_workload
+def ansor_depthwise(input_shape, filter_shape, dtype="float32"):
+    A = te.placeholder(input_shape, name="A", dtype=dtype)
+    B = te.placeholder(filter_shape, name="B", dtype=dtype)
+    C = topi.nn.depthwise_conv2d_nhwc(
+        A, B, stride=strides, padding=padding, dilation=dilation, out_dtype=dtype
+    )
+
+    return [A, B, C]
+
+
+## ---------------------------------------------
+
+
+def generate_ansor_template(log_file, target, trials):
+    task = tvm.auto_scheduler.SearchTask(
+        func=ansor_depthwise, args=(input_shape, filter_shape, "float32"), target=target
+    )
+
+    ## Set Parameters for Auto-Scheduler
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=trials,  # change this to 20000 to achieve the best performance
+        runner=auto_scheduler.LocalRunner(
+            number=10,
+            repeat=3,
+            timeout=100,
+            enable_cpu_cache_flush=True if target == "llvm -mcpu=a64fx" else False,
+        ),
+        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+        verbose=0,
+    )
+
+    start = time.time()
+    # Run auto-tuning (search)
+    task.tune(tune_option)
+    end = time.time()
+
+    time_avg, best_cfg = get_best_time(log_file)
+
+    print("Time spent:", time_avg)
+    print("Config:", best_cfg)
+    print("Time spent to search:", end - start)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "python print_record_info.py -m 'ansor' -a x86 -l 'results/cpu_matmul.json' -i 3"
+    )
+    parser.add_argument(
+        "-m", "--method", type=str, required=True, help="Options: ansor, droplet"
+    )
+    parser.add_argument(
+        "-a", "--arch", type=str, required=True, help="Options: x86, aarch64, cuda"
+    )
+    parser.add_argument("-l", "--logfile", type=str, required=True)
+    parser.add_argument("-t", "--trials", type=int, default=100)
+    args = parser.parse_args()
+
+    method = args.method
+    arch = args.arch
+    logfile = args.logfile
+    trials = args.trials
+
+    if arch == "x86":
+        target = tvm.target.Target("llvm")
+        dev = tvm.cpu()
+    elif arch == "cuda":
+        target = tvm.target.Target("cuda")
+        dev = tvm.cuda()
+    elif arch == "aarch64":
+        target = tvm.target.Target("llvm -mcpu=a64fx")
+        dev = tvm.cpu()
+    else:
+        print("Archtecture doesn't support.")
+        exit(0)
+
+    if method == "ansor":
+        generate_ansor_template(logfile, target, trials)
+    elif method == "droplet":
+        build_template("depthwise", logfile, target, trials)
